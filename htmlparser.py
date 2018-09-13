277e0ab3b7a6341828f8f4f136276137e1e045ea
nemo0806 <nemo0806@gmail.com>
2018-09-13 20:19:14 +0800

    test

---
+++ htmlparser.py
@@ -0,0 +1,24 @@
+from HTMLParser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+
+    def handle_starttag(self, tag, attrs):
+        print('<%s>' % tag)
+
+    def handle_endtag(self, tag):
+        print('</%s>' % tag)
+
+    def handle_startendtag(self, tag, attrs):
+        print('<%s/>' % tag)
+
+    def handle_data(self, data):
+        print('data')
+
+    def handle_comment(self, data):
+        print('<!-- -->')
+
+    def handle_entityref(self, name):
+        print('&%s;' % name)
+
+    def handle_charref(self, name):
+        print('&#%s;' % name)
